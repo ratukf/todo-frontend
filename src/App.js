1860ee4d0ed5e8c024c3c3c0a67d753a4ce5b0a2
Ratu <ratukf@gmail.com>
2026-02-11 14:39:40 +0700

    style(componen): show date as header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import { TodosList } from "./components/TodosList";
 import { AddTodo } from "./components/AddTodo";
 import { useState } from "react";
 import { TodoDetail } from "./components/TodoDetail";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+
+dayjs.extend(advancedFormat);
 
 function App() {
   const [id, setId] = useState(null);
   const [isTodoDetailOpen, setIsTodoDetailOpen] = useState(false);
+  const date = dayjs();
+  const formatted = date.format("ddd Do, MMMM");
+  const [weekdayDate, month] = formatted.split(", ");
 
   const handleOpenTodoDetail = (id) => {
     setId(id);
@@ -17,13 +24,14 @@ function App() {
   return (
     <Container className="App">
       <Grid container direction="column" alignItems="center" spacing={5}>
-        <Typography variant="h4" sx={{ my: 2 }}>
-          Todo List
+        <Typography
+          variant="h2"
+          sx={{ my: 2, fontWeight: "900", color: "black" }}
+        >
+          {weekdayDate}, <span style={{ color: "#888" }}>{month}</span>
         </Typography>
-
         {/* Add Todo Textfield */}
         <AddTodo />
-
         {/* List of todos */}
         <TodosList handleClick={handleOpenTodoDetail} />
       </Grid>
