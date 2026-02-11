46d9d52976597a08ce50618334330f8cfb55cb5f
Ratu <ratukf@gmail.com>
2026-02-11 12:08:26 +0700

    feat(component): create add to do

---
+++ src/components/AddTodo.js
@@ -0,0 +1,47 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import { useAddTodo } from "../hooks/useTodo";
+
+const AddTodo = ({ isOpen, handleClose }) => {
+  const addTodoMutation = useAddTodo();
+  const [title, setTitle] = useState("");
+
+  const handleAddTodo = () => {
+    if (!title.trim()) return;
+    addTodoMutation.mutate({ title, status: "pending" });
+    setTitle("");
+    handleClose();
+  };
+
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="add-todo-dialog-title"
+    >
+      <DialogTitle id="add-todo-dialog-title">Add New Todo</DialogTitle>
+      <DialogContent>
+        <TextField
+          fullWidth
+          label="Todo Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleAddTodo()}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleAddTodo}>Add</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export { AddTodo };
