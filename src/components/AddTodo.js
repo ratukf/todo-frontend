167ef5d4177e31e51469363b3981974c101b78c8
Ratu <ratukf@gmail.com>
2026-02-11 12:57:29 +0700

    style(component): change add to do component to be simple

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,13 +1,7 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  TextField,
-} from "@mui/material";
+import { InputAdornment, TextField } from "@mui/material";
 import { useState } from "react";
 import { useAddTodo } from "../hooks/useTodo";
+import { AddCircle } from "@mui/icons-material";
 
 const AddTodo = ({ isOpen, handleClose }) => {
   const addTodoMutation = useAddTodo();
@@ -21,26 +15,24 @@ const AddTodo = ({ isOpen, handleClose }) => {
   };
 
   return (
-    <Dialog
-      open={isOpen}
-      onClose={handleClose}
-      aria-labelledby="add-todo-dialog-title"
-    >
-      <DialogTitle id="add-todo-dialog-title">Add New Todo</DialogTitle>
-      <DialogContent>
-        <TextField
-          fullWidth
-          label="Todo Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && handleAddTodo()}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleAddTodo}>Add</Button>
-      </DialogActions>
-    </Dialog>
+    <>
+      <TextField
+        fullWidth
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && handleAddTodo()}
+        placeholder="Add new task"
+        slotProps={{
+          input: {
+            endAdornment: (
+              <InputAdornment position="end">
+                <AddCircle />
+              </InputAdornment>
+            ),
+          },
+        }}
+      />
+    </>
   );
 };
 
